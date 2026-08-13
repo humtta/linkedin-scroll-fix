04ce93cec282509802f31dfe01d4a25041c0bb2d
Hugo Marotta <humtta@proton.me>
2026-08-12 23:58:06 -0300

    Read manifest file

--- scripts/gen_script.py
+++ scripts/gen_script.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from json import loads
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
@@ -7,3 +8,5 @@ SRC_DIR = ROOT_DIR / "src"
 
 MANIFEST_FILE = SRC_DIR / "manifest.json"
 CSS_FILE = SRC_DIR / "inject.css"
+
+manifest = loads(MANIFEST_FILE.read_text(encoding="utf-8"))
