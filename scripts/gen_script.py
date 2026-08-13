1a77f7fd66e618baec7fe9a4035408e13583c027
Hugo Marotta <humtta@proton.me>
2026-08-13 12:08:17 -0300

    Set namespace

--- scripts/gen_script.py
+++ scripts/gen_script.py
@@ -12,5 +12,7 @@ CSS_FILE = SRC_DIR / "inject.css"
 ICON_FILE = ASSETS_DIR / "icon.svg"
 SCRIPT_FILE = SRC_DIR / "script.user.js"
 
+NAMESPACE = "https://github.com/humtta/linkedin-scroll-fix"
+
 manifest = loads(MANIFEST_FILE.read_text(encoding="utf-8"))
 css = CSS_FILE.read_text(encoding="utf-8")
