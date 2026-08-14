a25414b5b416b06e4e8478b064c9adddd1aa5af9
Hugo Marotta <humtta@proton.me>
2026-08-14 11:19:54 -0300

    Add namespace in gen-script

--- scripts/gen-script.sh
+++ scripts/gen-script.sh
@@ -11,3 +11,5 @@ CSS_FILE="${ROOT_DIR}/src/inject.css"
 # Paths relative to the project root directory
 ICON_FILE_REL="src/assets/icon.svg"
 SCRIPT_FILE_REL="script.user.js"
+
+NAMESPACE="https://github.com/humtta/linkedin-scroll-fix"
